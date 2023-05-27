ff583f868535aa75cede71799e268efa08696585
Luc-Saccoccio <lucsaccoccio@disroot.org>
2023-05-27 04:14:03 +0200

    Level parsing

--- level.go
+++ level.go
@@ -1,19 +1,31 @@
 package main
 
 import (
+  "strings"
 	"github.com/gen2brain/raylib-go/raylib"
 )
 
-/*
-trap :
-  - 0 : none
-  - 1 : poison
-  - 2 : enlève un mouvement
-*/
+const (
+  // Kindase '.' is the kind for a base tile
+  KindBase = iota
+  // KindPoison 'P' is the kind for a poison trap
+  KindPoison
+  // KindMovement 'M' is the kind of a movement trap
+  KindMovement
+  // KindWall '#' is the kind for a wall
+  KindWall
+)
+
+var RuneKinds = map[rune]Case{
+  '.' : Case{KindBase, true},
+  'P' : Case{KindPoison, false},
+  'M' : Case{KindMovement, false},
+  '#' : Case{KindWall, true},
+}
+
 type Case struct {
-	trap int8
-	wall bool
-	tile string
+	kind int
+  discovered bool
 }
 
 type Enemy struct {
@@ -24,6 +36,25 @@ type Enemy struct {
 }
 
 type Level struct {
-	cases   [][]Case
+	cases   map[int](map[int]Case)
 	enemies []Enemy
+}
+
+func ParseWorld(input string) Level {
+  var w Level
+  if w.cases == nil {
+    w.cases = make(map[int](map[int]Case))
+  }
+  for y, row := range strings.Split(strings.TrimSpace(input), "\n") {
+    for x, raw := range row {
+      kind, ok := RuneKinds[raw]
+      if ok {
+        if w.cases[x] == nil {
+          w.cases[x] = make(map[int]Case)
+        }
+        w.cases[x][y] = kind
+      }
+    }
+  }
+  return w
 }
