83068d29524ab829488c2832a000a03c9664f344
Luc-Saccoccio <lucsaccoccio@disroot.org>
2023-05-27 14:00:23 +0200

    Enemies Parsing

--- level.go
+++ level.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"math/rand"
+	"io/ioutil"
 	"strings"
+  "strconv"
+	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
 const (
@@ -14,6 +17,10 @@ const (
 	KindMovement
 	// KindWall '#' is the kind for a wall
 	KindWall
+  // KindEntry 'E' is the kind for the level entry
+  KindEntry
+  // KindExit 'S' is the kind for the level exit
+  KindExit
 )
 
 var RuneKinds = map[rune]Case{
@@ -21,6 +28,8 @@ var RuneKinds = map[rune]Case{
 	'P': {KindPoison, 0},
 	'M': {KindMovement, 0},
 	'#': {KindWall, 0},
+  'E': {KindEntry, 0},
+  'S': {KindExit, 0},
 }
 
 type Case struct {
@@ -33,24 +42,61 @@ type Level struct {
 	enemies []Enemy
 }
 
-func ParseWorld(input string) Level {
-	var w Level
-	if w.cases == nil {
-		w.cases = make(map[int](map[int]Case))
-	}
+func ParseWorld(input string) map[int](map[int]Case) {
+  cases := make(map[int](map[int]Case))
 	for y, row := range strings.Split(strings.TrimSpace(input), "\n") {
 		for x, raw := range row {
 			caseKind, ok := RuneKinds[raw]
 			if ok {
-				if w.cases[x] == nil {
-					w.cases[x] = make(map[int]Case)
+				if cases[x] == nil {
+					cases[x] = make(map[int]Case)
 				}
 				if caseKind.kind == KindBase {
 					caseKind.tile = rand.Intn(3)
 				}
-				w.cases[x][y] = caseKind
+				cases[x][y] = caseKind
 			}
 		}
 	}
-	return w
+	return cases
+}
+
+func readInt(s string) int8 {
+  n, _ := strconv.ParseInt(s, 10, 8)
+  return int8(n)
+}
+
+func readFloat(s string) float32 {
+  n, _ := strconv.ParseFloat(s, 32)
+  return float32(n)
+}
+
+func ParseEnemies(input string) []Enemy {
+  enemiesInput := strings.Split(strings.TrimSpace(input), "\n")
+  enemies := make([]Enemy, len(enemiesInput))
+  for y, row := range enemiesInput {
+    enemies[y] = Enemy{}
+    infos := strings.Split(strings.TrimSpace(row), " ")
+    enemies[y].sort = readInt(infos[0])
+    enemies[y].health = readInt(infos[1])
+    enemies[y].damage = readInt(infos[2])
+    enemies[y].aggroRange = readInt(infos[3])
+    enemies[y].pos = rl.NewVector2(readFloat(infos[4]), readFloat(infos[5]))
+  }
+  return enemies
+}
+
+func ParseLevel(casesFile string, enemiesFile string) (l Level, err error) {
+  levelContent, err := ioutil.ReadFile(casesFile)
+  if err != nil {
+    return l, err
+  }
+  l.cases = ParseWorld(string(levelContent))
+
+  enemiesContent, err := ioutil.ReadFile(enemiesFile)
+  if err != nil {
+    return l, err
+  }
+  l.enemies = ParseEnemies(string(enemiesContent))
+  return l, nil
 }
